b093d0a3d22cf1facce22e44a62bfe905528c205
David Pye <davidmpye@gmail.com>
2024-12-25 15:43:47 +0000

    Update to use separate embassy tasks

--- matrix-keyboard/fw/src/main.rs
+++ matrix-keyboard/fw/src/main.rs
@@ -4,18 +4,17 @@
 use core::sync::atomic::{AtomicBool, Ordering};
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_futures::join::join;
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Input, Pull};
 use embassy_rp::peripherals::USB;
 use embassy_rp::usb::{Driver, InterruptHandler};
-use embassy_usb::class::hid::{HidReaderWriter, ReportId, RequestHandler, State};
+use embassy_usb::class::hid::{HidWriter, HidReader, HidReaderWriter, ReportId, RequestHandler, State};
 use embassy_usb::control::OutResponse;
-use embassy_usb::{Builder, Config, Handler};
+use embassy_usb::{Builder, Config, Handler, UsbDevice};
 use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
 use {defmt_rtt as _, panic_probe as _};
 use embassy_rp::gpio::{Level, Output};
-
+use static_cell::StaticCell;
 use embassy_time::{Duration, Timer};
 
 bind_interrupts!(struct Irqs {
@@ -23,7 +22,7 @@ bind_interrupts!(struct Irqs {
 });
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());   
 
     // Create the driver, from the HAL.
@@ -37,28 +36,28 @@ async fn main(_spawner: Spawner) {
     config.max_power = 100;
     config.max_packet_size_0 = 64;
 
-    // Create embassy-usb DeviceBuilder using the driver and config.
-    // It needs some buffers for building the descriptors.
-    let mut config_descriptor = [0; 256];
-    let mut bos_descriptor = [0; 256];
-    // You can also add a Microsoft OS descriptor.
-    let mut msos_descriptor = [0; 256]; 
-    let mut control_buf = [0; 64];
-    let mut request_handler = MyRequestHandler {};
-    let mut device_handler = MyDeviceHandler::new();
+    static CONFIG_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+    static BOS_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+    static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
 
-    let mut state = State::new();
+    let request_handler = MyRequestHandler{};
+    static DEVICE_HANDLER:StaticCell<MyDeviceHandler> = StaticCell::new();
+    
+    let device_handler = DEVICE_HANDLER.init(MyDeviceHandler::new());
+
+    static STATE: StaticCell<State> = StaticCell::new();
+    let state = STATE.init(State::new());
 
     let mut builder = Builder::new(
         driver,
         config,
-        &mut config_descriptor,
-        &mut bos_descriptor,
-        &mut msos_descriptor,
-        &mut control_buf,
+        CONFIG_DESCRIPTOR.init([0; 256]),
+        BOS_DESCRIPTOR.init([0; 256]),
+        &mut [], // no msos descriptors
+        CONTROL_BUF.init([0; 64])
     );
 
-    builder.handler(&mut device_handler);
+    builder.handler(device_handler);
 
     // Create classes on the builder.
     let config = embassy_usb::class::hid::Config {
@@ -67,21 +66,45 @@ async fn main(_spawner: Spawner) {
         poll_ms: 60,
         max_packet_size: 64,
     };
-    let hid = HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut state, config);
+    let hid: HidReaderWriter<'_, Driver<'_, USB>, 1, 8> = HidReaderWriter::<_, 1, 8>::new(&mut builder, state, config);
 
-    // Build the builder.
-    let mut usb = builder.build();
-
-    // Run the USB device.
-    let usb_fut = usb.run();
+    // Build the builder - USB device will be run by usb_task
+    let mut usb: UsbDevice<'_, Driver<'_, USB>> = builder.build();
 
     //Description for the matrix keyboard and how the row/columns map to GPIOs
     let mut col_pins = [ Output::new(p.PIN_19, Level::Low), Output::new(p.PIN_20, Level::Low), Output::new(p.PIN_21, Level::Low) ] ;
-    let mut row_pins =  [ 
+    let row_pins =  [ 
         Input::new(p.PIN_0, Pull::Down),  Input::new(p.PIN_1, Pull::Down),Input::new(p.PIN_2, Pull::Down),Input::new(p.PIN_3, Pull::Down),
         Input::new(p.PIN_4, Pull::Down),  Input::new(p.PIN_5, Pull::Down),Input::new(p.PIN_6, Pull::Down),Input::new(p.PIN_7, Pull::Down),
     ];
 
+    let mut led_pin = Output::new(p.PIN_25, Level::Low);
+
+
+    let (reader, mut writer) = hid.split();
+
+    unwrap!(spawner.spawn(usb_task(usb)));
+    unwrap!(spawner.spawn(reader_task(reader, request_handler)));
+    unwrap!(spawner.spawn(writer_task(writer, col_pins, row_pins, led_pin)));
+}
+
+type MyUsbDriver = Driver<'static, USB>;
+type MyUsbDevice = UsbDevice<'static, MyUsbDriver>;
+type MyHidWriter = HidWriter<'static, MyUsbDriver, 8>;
+type MyHidReader = HidReader<'static, MyUsbDriver, 1>;
+
+#[embassy_executor::task]
+async fn usb_task(mut usb: MyUsbDevice) -> ! {
+    usb.run().await
+}
+
+#[embassy_executor::task]
+async fn reader_task(reader: MyHidReader, mut request_handler: MyRequestHandler)  -> ! {
+    reader.run(false, &mut request_handler).await;
+}
+
+#[embassy_executor::task]
+async fn writer_task(mut writer: MyHidWriter, mut col_pins: [Output<'static>;3], mut row_pins: [Input<'static>;8], mut led_pin: Output<'static>) -> ! {
     //This needs to match the same grid as the row_pins/col_pins above, or badness.
     let keypad_matrix = [ 
         [ 0x04u8, 0x05u8, 0x06u8, 0x07u8, 0x08u8, 0x09u8, 0x0Au8],  //A->G
@@ -89,33 +112,32 @@ async fn main(_spawner: Spawner) {
         [ 0x22u8, 0x23u8, 0x24u8, 0x25u8, 0x26u8, 0x28u8, 0x29u8],  //5->9, enter, escape
     ];
 
-    let (reader, mut writer) = hid.split();
+    loop {
+        let pressed_keys =  get_pressed_keys(&mut col_pins, &mut row_pins, keypad_matrix);
 
-    let in_fut = async {
-        loop {
-            let report = KeyboardReport {
-                keycodes: get_pressed_keys(&mut col_pins, &mut row_pins, keypad_matrix),
-                leds: 0,
-                modifier: 0,
-                reserved: 0,
-            };
-            // Send the report.
-           match writer.write_serialize(&report).await {
-                Ok(()) => {}
-                Err(e) => warn!("Failed to send report: {:?}", e),
-            }; 
-            Timer::after(Duration::from_millis(5)).await;
+        if pressed_keys != [0x00u8;6] {
+            //flash led
+            led_pin.set_high();
         }
-    };
+        let report = KeyboardReport {
+            keycodes: pressed_keys,
+            leds: 0,
+            modifier: 0,
+            reserved: 0,
+        };
 
-    let out_fut = async {
-       reader.run(false, &mut request_handler).await;
-    };
+        // Send the report.
+        match writer.write_serialize(&report).await {
+           Ok(()) => {}
+          Err(e) => warn!("Failed to send report: {:?}", e),
+        }; 
+        Timer::after(Duration::from_millis(5)).await;
+        
+        led_pin.set_low();
 
-    // Run everything concurrently.
-    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
-    join(usb_fut, join(in_fut, out_fut)).await;
+    }
 }
+
 
 fn get_pressed_keys(col_pins:&mut [Output;3], row_pins: &mut[Input;8], keypad_matrix:[[u8;7];3]) -> [u8;6] {
     let mut pressed_keys = [0x00u8;6];
