0c9d2842b98540f68118f9f18118b370718c5fb4
David Pye <davidmpye@gmail.com>
2025-01-22 21:41:30 +0000

    Update to create permanent task for handling vends

--- vmc-host/src/main.rs
+++ vmc-host/src/main.rs
@@ -3,7 +3,7 @@ use gtk4::glib::value;
 use lcd_driver::LcdDriver;
 
 mod vmc_driver;
-use vmc_driver::VmcDriver;
+use vmc_driver::{VmcDriver, VmcClientError};
 use vmc_icd::DispenserAddress;
 
 const KEYBOARD_DEVICE_NAME:&str = "matrix-keyboard";
@@ -17,6 +17,7 @@ use std::sync::OnceLock;
 
 use std::sync::{Arc,Mutex};
 
+use std::convert::Infallible;
 use async_channel::{Sender, Receiver};
 
 //Spawn a tokio runtime instance for the postcard-rpc device handlers
@@ -35,7 +36,35 @@ fn main() -> glib::ExitCode {
     application.run()
 }
 
-async fn handle_vend(key_receiver: Receiver<char>) {
+async fn spawn_vend_task()-> (Sender<DispenserAddress>, Receiver<Result<(),vmc_driver::VmcClientError<Infallible>>>  ) {
+    //Vend task runs on the tokio runtime.
+    //Two channels exist:
+    // one which you can send it dispenser addresses
+    // one you can read the result from your request
+    let (tx1, rx1) = async_channel::bounded(1); //TX *TO* client
+    let (tx2, rx2) = async_channel::bounded(1); //RX *FROM* client
+    runtime().spawn(clone!(
+        async move {
+            match VmcDriver::new(VMC_DEVICE_NAME) {
+                Ok(mut driver) => {
+                    loop {
+                        let addr = rx1.recv().await.unwrap();
+                        let result = driver.dispense(addr).await;
+                        tx2.send(result).await.expect("Vend channel failure");
+                    }
+                },
+                Err(msg) => {
+                    println!("VMC init failure: {}", msg);
+                    //need to send something down the channel..
+                    tx2.send(Err(vmc_driver::VmcClientError::Comms(postcard_rpc::host_client::HostErr::BadResponse))).await.expect("Vend channel failure");
+                },
+            }
+        }
+    ));
+    return (tx1, rx2);
+}
+
+async fn handle_vend(key_receiver: Receiver<char>, tx: Sender<DispenserAddress>, rx:Receiver<Result<(), vmc_driver::VmcClientError<Infallible>>>) {
     let mut row:Option<char> = None;
     let mut col: Option<char> = None;
 
@@ -60,35 +89,17 @@ async fn handle_vend(key_receiver: Receiver<char>) {
             //Valid rows and columns found
             let r = row.unwrap();
             let c = col.unwrap();
+            
             //Got valid row and column, time to vend!
-            let (sender, receiver) = async_channel::bounded(1);
-            //Spawn a Postcard-RPC vend request on the Tokio runtime
-            runtime().spawn(clone!(
-                async move {
-                    match VmcDriver::new(VMC_DEVICE_NAME) {
-                        Ok(mut driver) => {
-                            let result = driver.dispense(DispenserAddress{row: r, col: c}).await;
-                            sender.send(result).await.expect("Vend channel failure");
-                        },
-                        Err(msg) => {
-                            println!("VMC init failure: {}", msg);
-                            //need to send something down the channel..
-                            sender.send(Err(vmc_driver::VmcClientError::Comms(postcard_rpc::host_client::HostErr::BadResponse))).await.expect("Vend channel failure");
-                        },
-                    }
+            //Send the dispenser 
+            tx.send(DispenserAddress{row:r, col: c}).await;
+            while let Ok(response) = rx.recv().await {
+                if let Ok(()) = response {
+                    println!("Vend success");
+                } else {
+                    println!("Vend failed");
                 }
-            ));
-
-            //This runs on the local event loop, and receives the result from the vend postcard RPC command running on tokio
-            glib::spawn_future_local(async move {
-                while let Ok(response) = receiver.recv().await {
-                    if let Ok(()) = response {
-                        println!("Vend success");
-                    } else {
-                        println!("Vend failed");
-                    }
-                }
-            });
+            }
         }
     }
 }
@@ -112,8 +123,12 @@ fn build_ui(application: &gtk4::Application) {
     //The keyboard listener will listen for A-G, 0-9, tick (enter), cross (esc), and UP/DOWN arrows
     //from the membrane keypad on the vending machine (appears as USB keyboard)
     window.add_controller(keyboard_listener(key_sender));
-    //Spawn off the handle vend future, with a channel to receive key presses
-    glib::spawn_future_local(async move { handle_vend(key_receiver).await; });
+
+    glib::spawn_future_local(async move { 
+        let (tx, rx ) = spawn_vend_task().await;
+        //Spawn off the handle vend future, with a channel to receive key presses
+        glib::spawn_future_local(async move { handle_vend(key_receiver, tx, rx).await; });
+    });
 
     vbox.append(&address_label);
     vbox.append(&button);
