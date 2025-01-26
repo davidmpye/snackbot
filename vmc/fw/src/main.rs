80ab3126266dc4f0c3252002754fc3ab27333632
David Pye <davidmpye@gmail.com>
2025-01-26 23:03:11 +0000

    Roll changes back onto vmc-host branch

--- vmc/fw/src/main.rs
+++ vmc/fw/src/main.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![no_main]
 
-
 mod motor_driver;
 
 use embassy_sync::mutex::Mutex;
@@ -18,9 +17,12 @@ use embassy_usb::{Config as UsbConfig, Handler, UsbDevice};
 
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Pin};
-use embassy_rp::peripherals::USB;
+use embassy_rp::peripherals::{USB, PIO0};
 use embassy_rp::usb;
 use embassy_rp::usb::{Driver as UsbDriver, InterruptHandler as UsbInterruptHandler};
+use embassy_rp::pio;
+use embassy_time::{Duration, Timer, WithTimeout};
+
 
 use static_cell::{ConstStaticCell, StaticCell};
 
@@ -29,12 +31,18 @@ use vmc_icd::{
     MotorStatus,GetDispenserInfo, ENDPOINT_LIST, TOPICS_IN_LIST, TOPICS_OUT_LIST
 };
 
+use pio_9bit_uart_async::{PioUartRx, PioUartTx, PioUartTxProgram, PioUartRxProgram};
+use embedded_io_async::{Read, Write};
+use mdb_async::{MDBResponse, Mdb};
+
+use mdb_async::coin_acceptor::{CoinAcceptor, PollEvent, ChangerStatus};
+
 use crate::motor_driver::MotorDriver;
 use postcard_rpc::{
     define_dispatch,
     header::VarHeader,
     server::{
-        impls::embassy_usb_v0_3::{
+        impls::embassy_usb_v0_4::{
             dispatch_impl::{WireRxBuf, WireRxImpl, WireSpawnImpl, WireStorage, WireTxImpl},
             PacketBuffers,
         },
@@ -60,6 +68,7 @@ static MOTOR_DRIVER: Mutex<
 
 bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => UsbInterruptHandler<USB>;
+    PIO0_IRQ_0 => pio::InterruptHandler<PIO0>;
 });
 
 pub struct Context {
@@ -116,25 +125,25 @@ async fn main(spawner: Spawner) {
     // Create the driver, from the HAL.
     let driver = UsbDriver::new(p.USB, Irqs);
 
+    static CONFIG : StaticCell<embassy_usb::Config> = StaticCell::new();
     // Create embassy-usb Config
-    let mut config = UsbConfig::new(0xDEAD, 0xBEEF);
-
+    let config = CONFIG.init(UsbConfig::new(0xDEAD, 0xBEEF));
     config.manufacturer = Some("Snackbot");
     config.product = Some("vmc");
     config.serial_number = Some("12345678");
-
     config.device_class = 0xEF;
     config.device_sub_class = 0x02;
     config.device_protocol = 0x01;
     config.composite_with_iads = true;
 
-    static DEVICE_HANDLER: StaticCell<MyDeviceHandler> = StaticCell::new();
-
     let pbufs = PBUFS.take();
 
     //We use init_without_build because init consumes the driver, and creates (and doesn't return) a builder otherwise
     let (mut builder, tx_impl, rx_impl) =
-        STORAGE.init_without_build(driver, config, pbufs.tx_buf.as_mut_slice());
+        STORAGE.init_without_build(driver, *config, pbufs.tx_buf.as_mut_slice());
+
+    static DEVICE_HANDLER: StaticCell<MyDeviceHandler> = StaticCell::new();
+
     let device_handler = DEVICE_HANDLER.init(MyDeviceHandler::new());
     builder.handler(device_handler);
 
@@ -178,11 +187,45 @@ async fn main(spawner: Spawner) {
     }
 
     // Build the builder - USB device will be run by usb_task
-    let usb: UsbDevice<'_, UsbDriver<'_, USB>> = builder.build();
+    let usb = builder.build();
 
     //USB device handler task
     spawner.must_spawn(usb_task(usb));
 
+    // PIO UART setup for MDB
+    let pio::Pio {
+        mut common, sm0, sm1, ..
+    } = pio::Pio::new(p.PIO0, Irqs);
+    let tx_program = PioUartTxProgram::new(&mut common);
+    let uart_tx = PioUartTx::new(9600, &mut common, sm0, p.PIN_21, &tx_program);
+    let rx_program = PioUartRxProgram::new(&mut common);
+    let uart_rx = PioUartRx::new(9600, 25000, 2500, &mut common, sm1, p.PIN_20, &rx_program);
+    let mut mdb =Mdb::new(uart_tx, uart_rx);
+
+/*
+    match CoinAcceptor::init(&mut mdb).await {
+        Some(mut b) => {info!("Got {}",b);
+        b.enable_coins(&mut mdb, 0xffff).await;
+
+        loop {
+            for (num, e) in  b.poll(&mut mdb).await.iter().enumerate() {
+                match e {
+                    Some(event) => {
+                        match event {
+                            PollEvent::Status(ChangerStatus::EscrowPressed) => {debug!("Escrow pressed - event number {}", num);}
+                            PollEvent::Coin(x) => {debug!("Got a coin - event num {}",num);}
+                            _=> {},
+                        }
+                    },
+                     _ =>{},
+
+                }
+            }
+        }
+    },
+        _ => {},
+    }
+ */
     //Postcard server mainloop just runs here
     loop {
         let _ = server.run().await;
