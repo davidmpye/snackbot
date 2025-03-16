b92d7d8fa1390bdf309601be22dfa0d3ceebc747
David Pye <davidmpye@gmail.com>
2025-03-16 12:46:35 +0100

    As per last commit

--- vmc/vmc-icd/src/coinacceptor.rs
+++ vmc/vmc-icd/src/coinacceptor.rs
@@ -21,4 +21,39 @@ pub enum CoinRouting {
 #[derive(Serialize, Deserialize, Schema, Debug, PartialEq, Copy, Clone)]
 pub enum CoinAcceptorEvent {
     EscrowPressed,
+    PayoutBusy,
+    NoCredit,
+    DefectiveTubeSensor,
+    DoubleArrival,
+    AcceptorUnplugged,
+    TubeJam,
+    RomChecksumError,
+    CoinRoutingError,
+    Busy,
+    WasReset,
+    CoinJam,
+    AttemptedCoinRemoval,
+    InvalidEvent,
+}
+
+
+impl From<u8> for CoinAcceptorEvent {
+    fn from(byte:u8) -> Self {
+        match byte {
+            0x01 => CoinAcceptorEvent::EscrowPressed,
+            0x02 => CoinAcceptorEvent::PayoutBusy,
+            0x03 => CoinAcceptorEvent::NoCredit,
+            0x04 => CoinAcceptorEvent::DefectiveTubeSensor,
+            0x05 => CoinAcceptorEvent::DoubleArrival,
+            0x06 => CoinAcceptorEvent::AcceptorUnplugged,
+            0x07 => CoinAcceptorEvent::TubeJam,
+            0x08 => CoinAcceptorEvent::RomChecksumError,
+            0x09 => CoinAcceptorEvent::CoinRoutingError,
+            0x0A => CoinAcceptorEvent::Busy,
+            0x0B => CoinAcceptorEvent::WasReset,
+            0x0C => CoinAcceptorEvent::CoinJam,
+            0x0D => CoinAcceptorEvent::InvalidEvent,
+            _ => CoinAcceptorEvent::InvalidEvent,
+        }
+    }
 }
